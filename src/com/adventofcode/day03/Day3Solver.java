c06cbdd2356b15900b6edfe619d3ae9cd33acea8
franco <franco.avi@cosmanitalia.it>
2020-12-03 09:27:28 +0100

    :sparkles: day 3 solutions

--- src/com/adventofcode/day03/Day3Solver.java
+++ src/com/adventofcode/day03/Day3Solver.java
@@ -12,12 +12,26 @@ public class Day3Solver extends ChallengeSolver {
 
     @Override
     public void solve(List<String> input) {
+        System.out.println(countSlope(input, 3, 1));
+    }
 
+    private int countSlope(List<String> input, int right, int down) {
+        int treeCounter = 0;
+        for (int y = 0, x = 0; y < input.size(); y += down){
+            if (input.get(y).charAt(x) == '#') treeCounter++;
+            x = (x + right) % input.get(y).length();
+        }
+        return treeCounter;
     }
 
     @Override
     public void solvePartTwo(List<String> input) {
-
+        int result = countSlope(input, 1, 1);
+        result *= countSlope(input, 3, 1);
+        result *= countSlope(input, 5, 1);
+        result *= countSlope(input, 7, 1);
+        result *= countSlope(input, 1, 2);
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
